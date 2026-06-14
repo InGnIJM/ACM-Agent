5acff84d0862ca2a5e1455a4e2c8e155f5af2ff5
金铭 <14517833+jin-ming-05@user.noreply.gitee.com>
2026-06-14 18:55:59 +0800

    feat(backend): add auth guards

---
+++ backend/src/common/guards/optional-auth.guard.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class OptionalAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = any>(_err: any, user: TUser): TUser {
+    if (!user) {
+      return null as any;
+    }
+    return user;
+  }
+}
